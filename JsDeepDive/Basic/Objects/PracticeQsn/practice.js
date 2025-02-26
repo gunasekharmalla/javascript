98188ff81b4bcca87ca3cc279b1c0ea26b466657
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-26 16:51:17 +0530

    Update practice.js

---
+++ JsDeepDive/Basic/Objects/PracticeQsn/practice.js
@@ -0,0 +1,44 @@
+1.	Create an object car with properties:
+  o	brand: "Toyota"
+  o	model: "Corolla"
+  o	year: 2022
+  2.	Print the car’s brand using dot notation.
+  3.	Change the year to 2023.
+  4.	Add a new property color with the value "Red".
+  5.	Remove the model property.
+  6.	Use a for...in loop to print all properties.
+
+    ✅ Expected Output:
+        Toyota
+        brand: Toyota
+        year: 2023
+        color: Red
+__________________________________________________________
+📝 Practice Task
+1.	Create an object car with:
+  o	brand: "Tesla"
+  o	model: "Model X"
+  o	year: 2023
+  2.	Add a method info that prints:
+  o	"This is a Tesla Model X from 2023"
+  3.	Call the method.
+  4.	Add another method updateYear(newYear) to change the car’s year.
+  5.	Call updateYear(2024) and print the car object.
+✅ Expected Output:
+    { brand: 'Tesla', model: 'Model X', year: 2023 }
+    This is a Tesla Model X from 2023
+    { brand: 'Tesla', model: 'Model X', year: 2024, info: [Function], updateYear: [Function] }
+_______________________________________________________________
+📝 Task:
+1. Create an object phone with the following properties:
+  brand: "Samsung"
+  model: "Galaxy S24"
+  price: 80000
+  Add a method discount(percent) that reduces the price by the given percentage.
+  Example: phone.discount(10); should reduce the price by 10%.
+  Add another method getInfo() that returns a string in this format:
+  "Samsung Galaxy S24 costs ₹72000" (if the price is updated).
+    ✅ Expected Output:
+        Updated Price: 72000
+        Samsung Galaxy S24 costs ₹72000
+
