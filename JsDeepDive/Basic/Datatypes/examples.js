bbdda7152e4d49ca632f3f6e40b97dd96f40a734
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-01 21:17:06 +0530

    Update examples.js

---
+++ JsDeepDive/Basic/Datatypes/examples.js
@@ -0,0 +1,19 @@
+4. Variables & Data Types
+Variables store data. Use let, const, or var to declare them.
+
+
+let name = "Alice";      // String
+const age = 25;          // Number
+var isStudent = true;    // Boolean
+
+console.log(name, age, isStudent);
+
+🔹 let → Preferred for variables that change
+🔹 const → Preferred for constants (values that don’t change)
+🔹 var → Older way, avoid using it
+
+let name = "guna",age = 20;
+const learn = "Learning js";
+console.log(name, age, learn);
+
+guna 20 Learning js
